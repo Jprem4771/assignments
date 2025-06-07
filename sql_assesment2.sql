aa231322700ed4eb6f6541e604c30b952850a8c9
Jprem4771 <166198720+Jprem4771@users.noreply.github.com>
2025-06-07 13:31:14 +0530

    Add files via upload

---
+++ sql_assesment2.sql
@@ -0,0 +1,65 @@
+-- Create Database
+CREATE DATABASE try;
+USE try;
+CREATE TABLE employees (
+Employee_id INT AUTO_INCREMENT PRIMARY KEY, name VARCHAR (100),
+position VARCHAR (100), salary DECIMAL (10, 2), hire_date DATE
+);
+CREATE TABLE employee_audit (
+audit_id INT AUTO_INCREMENT PRIMARY KEY,
+employee_id INT,
+name VARCHAR
+(100),
+position VARCHAR (100), salary DECIMAL (10, 2), hire_date DATE,
+action_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+); 
+
+INSERT INTO employees (name, position, salary, hire_date) VALUES ('John Doe',
+'Software Engineer', 80000.00, '2022-01-15'),
+('Jane Smith', 'Project Manager', 90000.00, '2021-05-22'),
+('Alice Johnson', 'UX Designer', 75000.00, '2023-03-01');
+
+/*create a Trigger that will insert a record into the employee_audit table every time
+a new employee is added to the employee’s table:
+We have an employee’s table, and we want to create a stored procedure to add a
+new employee and automatically log this action in an
+employee_audit table.*/
+
+DELIMITER //
+
+CREATE TRIGGER after_employee_insert
+AFTER INSERT ON employees
+FOR EACH ROW
+BEGIN
+    INSERT INTO employee_audit (employee_id, name, position, salary, hire_date)
+    VALUES (NEW.Employee_id, NEW.name, NEW.position, NEW.salary, NEW.hire_date);
+END;
+//
+
+DELIMITER ;
+
+-- stored procedure
+
+DELIMITER //
+
+CREATE PROCEDURE AddEmployee (
+    IN emp_name VARCHAR(100),
+    IN emp_position VARCHAR(100),
+    IN emp_salary DECIMAL(10, 2),
+    IN emp_hire_date DATE
+)
+BEGIN
+    INSERT INTO employees (name, position, salary, hire_date)
+    VALUES (emp_name, emp_position, emp_salary, emp_hire_date);
+END;
+//
+
+DELIMITER ;
+
+CALL AddEmployee('Robert Miles', 'Data Analyst', 67000.00, '2024-11-20');
+
+
+select * from employee_audit;
+
+
+ 
